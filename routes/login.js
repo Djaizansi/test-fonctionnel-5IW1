ffa4acde6dd35f8f6f33e9af222bb5b2d2092c2d
Djaizansi <youcef.jallali@gmail.com>
2022-07-05 12:23:01 +0200

    Auth generate JWT

--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,45 @@
 const express = require('express');
 const router = express.Router();
+const models = require('../models');
+const User = models.User;
+const bcrypt = require('bcryptjs');
+const generateAccessToken = require("../jwt/generateAccesToken");
+
 /* POST login */
 router.post('/', function(req, res) {
-    //CODE LOGIN
+    const { email, password } = req.body;
+    if(email && password) {
+        User.findOne({
+            where: {email}
+        })
+            .then(user => {
+                if(user) {
+                    if(bcrypt.compareSync(password, user.password)) {
+                        const accessToken = generateAccessToken({
+                            id: user.id,
+                            email: user.email,
+                            first_name: user.first_name,
+                            last_name: user.last_name,
+                        });
+                        res.json({
+                            token: accessToken
+                        });
+                    } else {
+                        res.status(401).json({
+                            error: 'Invalid credentials (password)'
+                        });
+                    }
+                } else {
+                    res.status(401).json({
+                        error: 'Invalid credentials'
+                    });
+                }
+            })
+    }else {
+        res.status(400).json({
+            message: "Entrez un email et un mot de passe"
+        });
+    }
 });
 
 module.exports = router;
